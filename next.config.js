e445cd94ff26710dc686f73a11ccfa80933fc802
mavalero010 <maicolvpolo@gmail.com>
2025-03-29 15:30:29 -0500

    config next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig =
+{
   reactStrictMode: false,
-};
+  trailingSlash: true, 
+  output: "export", 
+  images: {
+    unoptimized: true, 
+  }
+}
 
 module.exports = nextConfig;
